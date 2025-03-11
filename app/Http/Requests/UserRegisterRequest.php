4ade78c2017c49d3ea93a949ef37f4c3aedb1da5
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-11 23:26:32 +0500

    for register request

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -11,7 +11,7 @@ class UserRegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UserRegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'=>'required|min:3',
+            'email'=> 'required',
+            'password'=> 'required|min:6',
+            'image'=>'required'
         ];
     }
 }
