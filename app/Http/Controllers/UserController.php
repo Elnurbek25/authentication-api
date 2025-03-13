18dbd6d2376696d12f65de15e201c08e4c6a2147
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-13 22:55:43 +0500

    created users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,63 +3,85 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Image;
+use App\Jobs\SendEmailJob;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserLoginRequest;
+use App\Mail\SendEmailNotification;
 use App\Http\Resources\UserResource;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Requests\UserRegisterRequest;
 
 class UserController extends Controller
 {
-    public function register(Request $request)
+    public function register(UserRegisterRequest $request)
     {
+
+        $image=$request->file('image');
+        $avatar=$this->uploadFile($image,'avatar');
+
         $user=User::create([
             'name'=>$request->name,
             'email'=>$request->email,
-            'password'=>bcrypt( $request->password),
+            'verification_token' => Str::random(64),
+            'path'=>$avatar,
+            'password'=>bcrypt($request->password),
         ]);
-    
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $imagePath = $this->uploadPhoto($image, 'images'); 
-    
-            Image::create([
-                'path' => $imagePath,
-                'imageable_id' => $user->id,
-                'imageable_type' => User::class, 
-            ]);
-        $token =$user->createToken('auth_token')->plainTextToken;
+        
+        SendEmailJob::dispatch($user);
+
         return response()->json([
             'success'=>true,
-            'token'=>$token,
-            'user'=> new UserResource($user->load('image'))
+            'message'=>'Emailingzini tekshiring'
         ]);
     }
-    }
-    public function login(Request $request)
+    public function login(UserLoginRequest $request)
     {
-        $user=User::where('email',$request->email)->first();
-    if(!$user || !Hash::check($request->password,$user->password))
-    {
-        return response()->json(
-            [
-                'success'=>false
-            ],401
-            );
-    }
-    $user->tokens()->delete();
-    $token=$user->createToken('auth_book')->plainTextToken;
-    return response()->json([
-        'success'=>true,
-        'token'=>$token,
-        'user'=> new UserResource($user->load('image'))
+        $credentials = $request->only('email', 'password');
 
-    ]);
-    }
-    public function logout(Request $request){
-        $user=$request->user();
-        $user->tokens->delete();
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            if (!$user->email_verified_at) {
+                Auth::logout();
+                return response()->json([
+                    'success'=>false,
+                    'message'=>'Emailingizni tasdiqlang'
+                ]);
+            }
+            $token=User::createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'success'=>true,
+                'token'=>$token,
+                'user'=> new UserResource($user)
+            ]);
+
+        }
+
         return response()->json([
-            'message'=>'Siz logout qildingiz'
+            'success'=>false,
+            'message'=>'Email yoki parol xato'
         ]);
     }
+    public function verifyEmail(Request $request)
+    {
+        $token = $request->query('token');
+        
+        $user = User::where('verification_token', $token)->first();
+
+        if (!$user) {
+            return response()->json([
+                'success'=>false,
+                'message'=>'Token not found'
+            ]);
+        }
+
+        $user->email_verified_at = now();
+        $user->save();
+
+        return response()->json([
+            'success'=>true,
+            'message'=>'Email verified'
+        ]);}
 }
