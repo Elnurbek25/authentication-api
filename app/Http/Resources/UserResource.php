230f725031d405541949e28e387d0a1a31e8e770
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-13 22:56:22 +0500

    for users resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,10 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'username'=>$this->name,
+            'email'=>$this->email,
+            'avatar' => $this->path,
+        ];
     }
 }
