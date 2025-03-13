4f09625e892c9325c904bcb478a4290922faff7a
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-13 22:57:09 +0500

    changed + image

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,17 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Models\Image;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -24,6 +22,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'path',
+        'verification_token'
     ];
 
     /**
@@ -47,9 +47,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-    public function image(): MorphOne
-    {
-    return $this->morphOne(Image::class, 'imageable');
     }
 }
