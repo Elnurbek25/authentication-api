1300eafa341d837e0c65b9232abc99671c371d35
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-13 22:56:52 +0500

    email notification

---
+++ app/Mail/SendEmailNotification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendEmailNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(protected User $user)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'AUTENTIHICATION',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+      $link='http://localhost:8000/verify-email?token='.$this->user->verification_token;
+      return new Content
+      (
+        view:'emails.verify',
+        with:[
+            'link'=>$link,
+            'user'=>$this->user
+        ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
