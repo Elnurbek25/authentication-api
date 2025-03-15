16638b97b9bd57bdf83da4565ad3fd24b86ea7c4
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-15 23:55:58 +0500

    protected user -> public

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -1,22 +1,28 @@
 <?php
 
 namespace App\Jobs;
+
 use App\Mail\SendEmailNotification;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendEmailJob implements ShouldQueue
 {
-    use Queueable;
-    protected $user;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user; 
 
     /**
      * Create a new job instance.
      */
     public function __construct($user)
     {
-        $this->user=$user;
+        $this->user = $user;
     }
 
     /**
